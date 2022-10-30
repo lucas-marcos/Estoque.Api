3b55534cf053e4e69181575354c9f227b85a8ad2
lucas.marcos <lucas_190201lmms@hotmail.com>
2022-10-30 12:27:03 -0300

    Mudança de banco de dados

--- Estoque.Api/Data/ApplicationDbContext.cs
+++ Estoque.Api/Data/ApplicationDbContext.cs
@@ -11,4 +11,15 @@ public class ApplicationDbContext : DbContext
     {
         
     }
+    
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+          
+        foreach (var property in builder.Model.GetEntityTypes()
+                     .SelectMany(t => t.GetProperties())
+                     .Where(p => p.ClrType == typeof(decimal) || p.ClrType == typeof(decimal?)))
+        {
+            property.SetColumnType("decimal(18, 6)");
+        }
+    }
 }
--- Estoque.Api/Program.cs
+++ Estoque.Api/Program.cs
@@ -39,11 +39,11 @@ builder.Services.AddTransient<IProdutoRepository, ProdutoRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+// if (app.Environment.IsDevelopment())
+// {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+// }
 
 app.UseHttpsRedirection();
 
