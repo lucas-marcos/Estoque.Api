6ff266bfc19fbb408aaf4c74e4005a8938279fd2
luizinhoh2o1 <alessandroluizfacul@gmail.com>
2022-10-31 20:21:49 -0300

    Nome do produto renomeado para Descricao, foi adicionado Tipo para o produto, CORS desativado

---
+++ Estoque.Api/Migrations/20221030232524_TipoDoProdutoAdicionadoENomeMudadoParaDescricao.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Estoque.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class TipoDoProdutoAdicionadoENomeMudadoParaDescricao : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Nome",
+                table: "Produto",
+                newName: "Tipo");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Preco",
+                table: "Produto",
+                type: "decimal(18,6)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Descricao",
+                table: "Produto",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Descricao",
+                table: "Produto");
+
+            migrationBuilder.RenameColumn(
+                name: "Tipo",
+                table: "Produto",
+                newName: "Nome");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Preco",
+                table: "Produto",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,6)");
+        }
+    }
+}
